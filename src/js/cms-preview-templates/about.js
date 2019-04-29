9967c9002a760ed1f94f9a3da31ab6f6898bd5ae
Aaron Roberts <Lumbearjack@gmail.com>
2019-04-29 01:31:37 -0400

    update about block styles

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -3,45 +3,41 @@ import { List } from 'immutable';
 
 import Jumbotron from "./components/jumbotron";
 
-const MediaBlock = ({heading, text, imageUrl, reverse}) => {
+const MediaBlock = ({heading, body, imageUrlA, reverse}) => {
   const imageContainerClassName = reverse
     ? "ph3-m w-50-m"
     : "ph3-m w-50-m order-last-m";
   return <div className="flex-m mhn3-m mb4">
     <div className={imageContainerClassName}>
-      <img src={imageUrl} alt="" className="db mb2" />
+      <img src={imageUrlA} alt="" className="db mb2" />
     </div>
     <div className="ph3-m w-50-m">
       <h3 className="f3 b lh-title mb1">{heading}</h3>
-      <p>{text}</p>
+      {body}
     </div>
   </div>;
 };
 
 export default class ValuesPreview extends React.Component {
   render() {
-    const {entry, widgetFor, getAsset} = this.props;
-    
+    const {entry, getAsset} = this.props;
+
     let image = getAsset(entry.getIn(["data", "image"]));
-
-    // const bodyText = widgetFor("body");
-
-    // console.log(bodyText);
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
       image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
-    
-    const entryValues = entry.getIn(["data", "values"]);
+
+    const entryValues = entry.getIn(["data", "about_entries"]);
     const values = entryValues ? entryValues.toJS() : [];
-    
+
     return <div>
       <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
       <div className="bg-off-white pv4">
         <div className="mw7 center ph3 pt4">
-          {values.map(({text, heading, imageUrl}, i) =>
-            <MediaBlock key={i} text={text} heading={heading} imageUrl={imageUrl} reverse={i % 2 === 0} />
+          {values.map(({body, heading, imageUrlA}, i) =>
+            <MediaBlock key={i} body={body} heading={heading} imageUrlA={imageUrlA} reverse={i % 2 === 0} />
           )}
         </div>
       </div>
