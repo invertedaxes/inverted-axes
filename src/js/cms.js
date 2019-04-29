9afcbb2deeda2a45bb6e7f6f48855eb75dcfdbc7
Aaron Roberts <Lumbearjack@gmail.com>
2019-04-29 00:27:32 -0400

    removing unused stuff

--- src/js/cms.js
+++ src/js/cms.js
@@ -3,10 +3,7 @@ import CMS from "netlify-cms";
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
 import AboutPreview from "./cms-preview-templates/about";
-import ContactPreview from "./cms-preview-templates/contact";
-
 
 // Example of creating a custom color widget
 class ColorControl extends React.Component {
@@ -23,7 +20,5 @@ class ColorControl extends React.Component {
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("about", AboutPreview);
-CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.registerWidget("color", ColorControl);
